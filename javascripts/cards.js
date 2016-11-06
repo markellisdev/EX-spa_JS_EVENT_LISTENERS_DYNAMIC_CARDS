571f677bd6a6e3af6cf4fe06236a4ff869fa5410
Mark Ellis <markellisdev@gmail.com>
2016-11-06 12:46:10 -0600

    delete function working and exercise complete

--- javascripts/cards.js
+++ javascripts/cards.js
@@ -1,30 +1,15 @@
 "use strict";
 
-// 1. Create an HTML page that contains a text area and a button labeled *Create*.
-
-
-
-// 1. When the user enters in text into the text area and then clicks the create button, create a new card element in the DOM. You decide the height/width of the card.
-
-
-
-// 1. When the user clicks the *Delete* button, the containing card, and no other cards, should then be removed from the DOM. Not just made invisible, actually [removed](https://developer.mozilla.org/en-US/docs/Web/API/Node/removeChild) from the DOM.
 /* ------------- VARIABLES --------------*/
-var create = document.getElementById("createButton");
+var create = document.getElementById("createButton"); //This variable makes it easy to attach event listener to create button.
 
-var deleteButton = document.getElementById("deleteButton");
+var textInput = document.getElementById("textArea"); //This variable makes to access the text input area.
 
-var textInput = document.getElementById("textArea");
-
-console.log("textinput", textInput);
+var counter = 1; //This counter is set so that a unique id can be added as cards are created.
 
 /* ------------- LISTENERS --------------*/
 create.addEventListener("click", createCard);
 
-deleteButton.addEventListener("click", deleteCard);
-
-
-// textInput.addEventListener("keyup", function(event));
 
 /* ------------- FUNCTIONS --------------*/
 // When the user enters in text into the text area and then clicks the create button, create a new card element in the DOM. You decide the height/width of the card.
@@ -34,19 +19,28 @@ function createCard () {
 	var xButton = document.createElement("button");
 	var cardText = document.createElement("p");
 	newCard.className = "w3-card";
-	newCard.id = "firstCard";
+	newCard.id = "card-" + counter++;
 	cardText.innerHTML = textInput.value;
 	cardText.className = "cardInfo";
 	xButton.id = "deleteMe";
-	xButton.addEventListener("click", deleteCard);
+	newCard.addEventListener("click", function(click) {
+		    console.log("e.target: ", event.target);
+		    console.log("e.currentTarget: ", event.currentTarget);
+		if (event.target.id === "deleteMe") {
+			deleteCard(event.currentTarget);
+		}
+	});
 
 	document.getElementsByTagName("body")[0].appendChild(newCard).appendChild(cardText).appendChild(xButton);
 
+	return counter;
 }
+// 1. When the user clicks the *Delete* button, the containing card, and no other cards, should then be removed from the DOM. Not just made invisible, actually [removed](https://developer.mozilla.org/en-US/docs/Web/API/Node/removeChild) from the DOM.
 
 function deleteCard () {
-	var parent = document.getElementsByTagName("body");
-	var child = document.getElementById("firstCard");
-	// parent.removeChild(child);
-	document.getElementById("firstCard").remove();
+
+	// document.event.currentTarget.remove;
+
+
+	event.currentTarget.remove();
 }
